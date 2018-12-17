4dd8852afb8256ec52f2c8a5350acba90e71e81b
liuhuihao <1061036802@qq.com>
2018-12-17 20:57:59 +0800

    feat: 1. Data bind.

--- dist/1-dataBind/index.js
+++ dist/1-dataBind/index.js
@@ -2,4 +2,15 @@
 
 var data = [1, 2, 3];
 
-var p = d3.select("body").selectAll("p").data(data).enter().append("p").text("hello");
+// const p = d3.select("body")
+//     .selectAll("p")
+//     .data(data)
+//     .enter()
+//     .append("p")
+//     .text("hello");
+
+
+var p = d3.select("body").selectAll("p").data(data).enter().append("p").text(function (item, i) {
+    console.log(item + "/" + i);
+    return item;
+});
--- src/1-dataBind/index.js
+++ src/1-dataBind/index.js
@@ -1,8 +1,19 @@
 const data = [1, 2, 3];
 
+// const p = d3.select("body")
+//     .selectAll("p")
+//     .data(data)
+//     .enter()
+//     .append("p")
+//     .text("hello");
+
+
 const p = d3.select("body")
     .selectAll("p")
     .data(data)
     .enter()
     .append("p")
-    .text("hello");
+    .text((item, i) => {
+        console.log(item + "/" + i);
+        return item;
+    });
